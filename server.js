c58dff897fcd91c2c8d5e6121d7c745f0b8d8352
Glitch (hello-express) <none>
2021-04-22 16:26:22 +0000

    🏮👓 Checkpoint ./server.js:31214192/4249

--- server.js
+++ server.js
@@ -1,30 +1,18 @@
-const Immer = require('immer');
+const Immer = require("immer");
 const express = require("express");
 const app = express();
-const http = require('http');
+const http = require("http");
 const server = http.createServer(app);
-const io = require('socket.io')(server);
+const io = require("socket.io")(server);
 
 const puzzle = {
   stars: 1,
   size: 5,
   borders: {
-    rows: [
-      '#|#||#',
-      '#|#|##',
-      '####|#',
-      '#|#||#',
-      '#||||#',
-    ],
-    columns: [
-      '#|#||#',
-      '#||#|#',
-      '##|###',
-      '######',
-      '#||###',
-    ],
+    rows: ["#|#||#", "#|#|##", "####|#", "#|#||#", "#||||#"],
+    columns: ["#|#||#", "#||#|#", "##|###", "######", "#||###"]
   }
-}
+};
 
 // make all the files in 'public' available
 // https://expressjs.com/en/starter/static-files.html
@@ -35,46 +23,51 @@ app.get("/", (request, response) => {
   response.sendFile(__dirname + "/views/index.html");
 });
 
-let globalBoard = new Array(5).fill().map((_, i) =>
-  new Array(5).fill().map((_, j) =>
-    ({ color: 'white', icon: '' })
-  )
-)
+let globalBoard = new Array(5)
+  .fill()
+  .map((_, i) =>
+    new Array(5).fill().map((_, j) => ({ color: "white", icon: "" }))
+  );
 
 const snapshots = [];
 
-io.on('connection', (socket) => {
-  socket.emit('puzzle', puzzle);
-  socket.emit('board', globalBoard);
-  socket.emit('snapshots', snapshots);
+io.on("connection", socket => {
+  socket.emit("puzzle", puzzle);
+  socket.emit("board", globalBoard);
+  socket.emit("snapshots", snapshots);
 
-  socket.on('click', ({ row, column, action }) => {
+  socket.on("click", ({ row, column, action }) => {
     globalBoard = Immer.produce(globalBoard, board => {
       const square = board[row][column];
       switch (action) {
-        case 'star':
-        case 'cross':
-          square.icon = square.icon === action ? '' : action;
+        case "star":
+        case "cross":
+          square.icon = square.icon === action ? "" : action;
           break;
         default:
-          square.color = square.color === action ? '' : action;
+          square.color = square.color === action ? "" : action;
           break;
       }
     });
-    io.emit('board', globalBoard);
+    io.emit("board", globalBoard);
   });
 
-  socket.on('takeSnapshot', () => {
+  socket.on("takeSnapshot", () => {
     snapshots.push(globalBoard);
-    
-    io.emit('snapshots', snapshots);
+
+    io.emit("snapshots", snapshots);
   });
 
-  socket.on('restoreSnapshot', (board) => {
+  socket.on("restoreSnapshot", board => {
     snapshots.push(globalBoard);
     globalBoard = board;
-    io.emit('board', globalBoard);
-    io.emit('snapshots', snapshots);
+    io.emit("board", globalBoard);
+    io.emit("snapshots", snapshots);
+  });
+
+  socket.on("reset", () => {
+    snapshots.splice(0, snapshots.length);
+    io.emit("snapshots", snapshots)
   });
 });
 
