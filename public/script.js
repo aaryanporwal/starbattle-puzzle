10c2bc4dca7052c111234e271b93f7e2779a2cfd
Glitch (star-battle-puzzle-party) <none>
2021-04-27 18:33:02 +0000

    🎮🚿 Checkpoint ./public/script.js:259113/17483

--- public/script.js
+++ public/script.js
@@ -7,63 +7,57 @@ const e = React.createElement;
 
 const Cross = ({ size }) =>
   e(
-    'svg',
-    {
-      width: size,
-      height: size,
-    },
-    e(
-      'line',
-      {
-        x1: 0,
-        y1: 0,
-        x2: size,
-        y2: size,
-        stroke: "#FF0000",
-        strokeWidth: size / 5
-      }
-    ),
-    e(
-      'line',
-      {
-        x1: 0,
-        y1: size,
-        x2: size,
-        y2: 0,
-        stroke: "#FF0000",
-        strokeWidth: size / 5
-      }
-    ),
-  )
-
-const Star = ({ size }) =>
-  e(
-    'svg',
+    "svg",
     {
       width: size,
       height: size
     },
-    e(
-      'polygon',
-      {
-        // points of the star lie on a circle with radius size / 2
-        // with an angle of 2/5 pi between each
-        // to draw the star, sweep an angle by 4/5 pi so we hit every other point
-        // point 0 at angle 0 is the middle right of the circle
-        // so the top point is point 3 at angle 12/5 pi = 2/5 pi
-        // but this results in a rotated star; we want the top point at 1/2 pi
-        // so subtract 1/10 pi to rotate the star so the top is at 1/2 pi
-        points: new Array(5).fill().map((_, i) => {
+    e("line", {
+      x1: 0,
+      y1: 0,
+      x2: size,
+      y2: size,
+      stroke: "#FF0000",
+      strokeWidth: size / 5
+    }),
+    e("line", {
+      x1: 0,
+      y1: size,
+      x2: size,
+      y2: 0,
+      stroke: "#FF0000",
+      strokeWidth: size / 5
+    })
+  );
+
+const Star = ({ size }) =>
+  e(
+    "svg",
+    {
+      width: size,
+      height: size
+    },
+    e("polygon", {
+      // points of the star lie on a circle with radius size / 2
+      // with an angle of 2/5 pi between each
+      // to draw the star, sweep an angle by 4/5 pi so we hit every other point
+      // point 0 at angle 0 is the middle right of the circle
+      // so the top point is point 3 at angle 12/5 pi = 2/5 pi
+      // but this results in a rotated star; we want the top point at 1/2 pi
+      // so subtract 1/10 pi to rotate the star so the top is at 1/2 pi
+      points: new Array(5)
+        .fill()
+        .map((_, i) => {
           const theta = (4 / 5) * Math.PI * i - (1 / 10) * Math.PI;
-          const x = size / 2 + Math.cos(theta) * size / 2;
-          const y = size / 2 + Math.sin(theta) * size / 2;
-          return `${x} ${y}`
-        }).join(' '),
-        stroke: "black",
-        strokeWidth: "1"
-      }
-    )
-  )
+          const x = size / 2 + (Math.cos(theta) * size) / 2;
+          const y = size / 2 + (Math.sin(theta) * size) / 2;
+          return `${x} ${y}`;
+        })
+        .join(" "),
+      stroke: "black",
+      strokeWidth: "1"
+    })
+  );
 
 const colorSelected = {
   green: "hsl(120, 50%, 50%)",
@@ -81,14 +75,24 @@ function borderStyle(wall) {
   return wall === "#" ? "solid blue 2px" : "solid black 1px";
 }
 
-const Square = ({ size, row, column, state, onClick, top, bottom, left, right }) => {
+const Square = ({
+  size,
+  row,
+  column,
+  state,
+  onClick,
+  top,
+  bottom,
+  left,
+  right
+}) => {
   const { color, icon } = state;
 
   return e(
     "div",
     {
       style: {
-        display: 'grid',
+        display: "grid",
         backgroundColor: colorSelected[color],
         borderTop: borderStyle(top),
         borderBottom: borderStyle(bottom),
@@ -98,14 +102,17 @@ const Square = ({ size, row, column, state, onClick, top, bottom, left, right })
       onClick
     },
     e(
-      "div",{
+      "div",
+      {
         style: {
-          margin: 'auto',
+          margin: "auto"
         }
       },
-      icon === 'cross' ? e(Cross, { size: size / 2 }) :
-      icon === 'star' ? e(Star, { size: size / 2 }) :
-      null
+      icon === "cross"
+        ? e(Cross, { size: size / 2 })
+        : icon === "star"
+        ? e(Star, { size: size / 2 })
+        : null
     )
   );
 };
@@ -121,7 +128,9 @@ const Board = ({ puzzle, board, size, makeOnClick }) => {
       const right = rows[row][column + 1];
       const state = board[row][column];
       const onClick = makeOnClick(row, column);
-      children.push(e(Square, { size, state, onClick, top, bottom, left, right }));
+      children.push(
+        e(Square, { size, state, onClick, top, bottom, left, right })
+      );
     }
   }
 
@@ -132,9 +141,9 @@ const Board = ({ puzzle, board, size, makeOnClick }) => {
         display: "grid",
         gridTemplateColumns: `repeat(5, ${size}px)`,
         gridTemplateRows: `repeat(5, ${size}px)`,
-        border: borderStyle('#'),
+        border: borderStyle("#"),
         // without an explicit maxWidth, the grid takes up the whole width ?
-        maxWidth: `${5 * size + 4}px`, // +4 to account for border
+        maxWidth: `${5 * size + 4}px` // +4 to account for border
       }
     },
     children
@@ -142,45 +151,44 @@ const Board = ({ puzzle, board, size, makeOnClick }) => {
 };
 
 const Tool = ({ action, setAction, selected }) => {
+  let button;
   switch (action) {
     case "star":
     case "cross":
-      return e(
+      button = e(
         "div",
         {
           style: {
-            display: 'grid'
-          },
-          onClick: () => setAction(action)
-        },
-        e(
-          "div",
-          {
-            style: {
-              margin: "auto",
-            }
-          },
-          e(action === 'star' ? Star : Cross, { size: 25 })
-        )
-      );
-    default:
-      return e(
-        "div",
-        {
-        },
-        e(
-          'div',
-          {
-            style: {
-              backgroundColor: selected
-                ? colorSelected[action]
-                : colorUnselected[action]
-            },
-            onClick: () => setAction(action)
+            margin: "auto"
           }
-        )
+        },
+        e(action === "star" ? Star : Cross, { size: 25 })
       );
+      break;
+
+    default:
+      button = e("div", {
+        style: {
+          margin: "auto",
+          backgroundColor: colorSelected[action],
+          width: "25px",
+          height: "25px"
+        },
+        onClick: () => setAction(action)
+      });
   }
+
+  return e(
+    "div",
+    {
+      style: {
+        display: "grid",
+        backgroundColor: selected ? "lightgray" : "white"
+      },
+      onClick: () => setAction(action)
+    },
+    button
+  );
 };
 
 const Toolbar = ({ action, setAction }) =>
@@ -220,8 +228,6 @@ const SnapshotButton = ({ takeSnapshot }) =>
     "Take snapshot"
   );
 
-
-
 const Snapshots = ({ puzzle, snapshots, takeSnapshot, restoreSnapshot }) =>
   e(
     "div",
@@ -251,16 +257,17 @@ const Snapshots = ({ puzzle, snapshots, takeSnapshot, restoreSnapshot }) =>
     )
   );
 
-const Reset = ({reset}) => 
-e(
-"button", {
-  style:{
-    margin: "10px"
-  },
-  onClick: reset
-},
-  "Reset Game"
-)
+const Reset = ({ reset }) =>
+  e(
+    "button",
+    {
+      style: {
+        margin: "10px"
+      },
+      onClick: reset
+    },
+    "Reset Game"
+  );
 
 const Title = () => e("h1", {}, "Star Battle Puzzle Party");
 
@@ -306,21 +313,26 @@ const App = () => {
     if (!socket.current) return;
     socket.current.emit("restoreSnapshot", board);
   };
-  
-  const reset = () =>{
-    socket.current.emit("reset")
-  }
+
+  const reset = () => {
+    socket.current.emit("reset");
+  };
 
   return e(
     "div",
-    {},
+    {
+      style: {
+        display: 'flex',
+        flexDirection: 'column'
+      }
+    },
     e(Title),
     e(Toolbar, { action, setAction }),
     puzzle && board
       ? e(Board, { action, puzzle, board, size: 100, makeOnClick })
       : null,
     e(Snapshots, { puzzle, snapshots, takeSnapshot, restoreSnapshot }),
-    e(Reset, {reset})
+    e(Reset, { reset })
   );
 };
 
