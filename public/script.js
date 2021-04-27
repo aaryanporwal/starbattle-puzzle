9f1b80695f16f442358750a1fde810e83fb19dd9
Glitch (star-battle-puzzle-party) <none>
2021-04-27 17:35:49 +0000

    👟🐘 Checkpoint ./public/script.js:259113/431

--- public/script.js
+++ public/script.js
@@ -49,13 +49,12 @@ const Star = ({ size }) =>
         // points of the star lie on a circle with radius size / 2
         // with an angle of 2/5 pi between each
         // to draw the star, sweep an angle by 4/5 pi so we hit every other point
-        // the point at angle 0 is at the middle right of the circle
-        // so the top point is at
-        // but this results in a rotated star
-        // so rotate back by half the distance between two points of the star
-        // or 1/5 pi
+        // point 0 at angle 0 is the middle right of the circle
+        // so the top point is point 3 at angle 12/5 pi = 2/5 pi
+        // but this results in a rotated star; we want the top point at 1/2 pi
+        // so subtract 1/10 pi to rotate the star so the top is at 1/2 pi
         points: new Array(5).fill().map((_, i) => {
-          const theta = (4 / 5) * Math.PI * i - (0/ 10) * Math.PI;
+          const theta = (4 / 5) * Math.PI * i - (1 / 10) * Math.PI;
           const x = size / 2 + Math.cos(theta) * size / 2;
           const y = size / 2 + Math.sin(theta) * size / 2;
           return `${x} ${y}`
