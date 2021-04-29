1f54e683aa411553f8a4c5e414b52b5937adbf8e
Glitch (star-battle-puzzle-party) <none>
2021-04-29 16:55:27 +0000

    📅🍟 Checkpoint ./server.js:259113/7367

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ const http = require("http");
 const server = http.createServer(app);
 const io = require("socket.io")(server);
 
-let current_puzzle = '1 star 5x5 #2';
+let current_puzzle = "2 star 10x10 #1";
 const puzzles = {
-  '1 star 5x5 #1': {
+  "1 star 5x5 #1": {
     stars: 1,
     size: 5,
     regions: [
@@ -15,10 +15,10 @@ const puzzles = {
       [0, 0, 2, 2, 1],
       [3, 0, 2, 1, 1],
       [3, 3, 4, 4, 4],
-      [3, 3, 3, 3, 3],
-    ],
+      [3, 3, 3, 3, 3]
+    ]
   },
-  '1 star 5x5 #2': {
+  "1 star 5x5 #2": {
     stars: 1,
     size: 5,
     regions: [
@@ -26,8 +26,25 @@ const puzzles = {
       [0, 1, 1, 2, 2],
       [3, 1, 4, 2, 2],
       [3, 3, 4, 2, 2],
-      [3, 3, 2, 2, 2],
+      [3, 3, 2, 2, 2]
+    ]
+  },
+  "2 star 10x10 #1": {
+    stars: 2,
+    size: 10,
+    regions: [
+      [0, 0, 1, 2, 2, 3, 3, 3, 4, 4],
+      [0, 0, 1, 2, 2, 3, 3, 3, 4, 4],
+      [0, 0, 1, 1, 2, 5, 5, 3, 4, 4],
+      [0, 0, 1, 1, 2, 2, 5, 5, 5, 6],
+      [0, 0, 1, 1, 2, 2, 5, 5, 5, 6],
+      [7, 0, 0, 1, 1, 5, 5, 6, 6, 6],
+      [7, 7, 7, 1, 1, 1, 1, 6, 6, 6],
+      [7, 7, 7, 7, 8, 8, 8, 8, 8, 8],
+      [9, 9, 9, 9, 9, 9, 8, 8, 8, 8],
+      [9, 9, 9, 9, 9, 9, 8, 8, 8, 8]
     ],
+    attribution: "https://www.puzzle-star-battle.com/?e=NTozLDM3Miw0MjQ="
   }
 };
 
@@ -40,19 +57,18 @@ app.get("/", (request, response) => {
   response.sendFile(__dirname + "/views/index.html");
 });
 
-let globalBoard = new Array(5)
-  .fill()
-  .map((_, i) =>
-    new Array(5).fill().map((_, j) => ({ color: "white", icon: "" }))
-  );
+let globalBoard;
 
-const clearGlobalBoard = () => {
-  globalBoard = new Array(5)
+function initializeGlobalBoard(puzzleName) {
+  const puzzle = puzzles[puzzleName];
+  globalBoard = new Array(puzzle.size)
     .fill()
     .map((_, i) =>
-      new Array(5).fill().map((_, j) => ({ color: "white", icon: "" }))
+      new Array(puzzle.size).fill().map((_, j) => ({ color: "white", icon: "" }))
     );
-};
+}
+
+initializeGlobalBoard(current_puzzle);
 
 const snapshots = [];
 
@@ -92,7 +108,7 @@ io.on("connection", socket => {
 
   socket.on("reset", () => {
     snapshots.splice(0, snapshots.length);
-    clearGlobalBoard();
+    initializeGlobalBoard(current_puzzle);
     io.emit("snapshots", snapshots);
     io.emit("board", globalBoard);
   });
